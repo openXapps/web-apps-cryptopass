6f9dc3d17f1ff9ec2978ae9156f6b9fb9a190a66
openXapps <gavin.dalton1@gmail.com>
2022-08-05 09:47:43 +0200

    Implemented Save As

--- src/routes/Download.jsx
+++ src/routes/Download.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { saveAs } from 'file-saver';
 
 import Container from '@mui/material/Container';
 import Paper from '@mui/material/Paper';
@@ -15,12 +16,19 @@ import { copyToClipboard } from '../helpers/utilities';
 function Download() {
   const rrNavigate = useNavigate();
   const [isCopied, setIsCopied] = useState(false);
+  const [isSaved, setIsSaved] = useState(false);
   const passwords = getDownloadableData();
 
   const handleCopyButton = () => {
     copyToClipboard(passwords).finally(() => {
       setIsCopied(true);
     });
+  };
+
+  const handleSaveAsButton = () => {
+    var blob = new Blob([passwords], { type: 'text/plain;charset=utf-8' });
+    saveAs(blob, 'CryptoPASS.json', { autoBom: false });
+    setIsSaved(true);
   };
 
   return (
@@ -43,6 +51,12 @@ function Download() {
             onClick={handleCopyButton}
             disabled={isCopied}
           >{isCopied ? 'Copied' : 'Copy'}</Button>
+          <Button
+            variant="outlined"
+            fullWidth
+            onClick={handleSaveAsButton}
+            disabled={isSaved}
+          >Save As</Button>
           <Button
             variant="outlined"
             fullWidth
